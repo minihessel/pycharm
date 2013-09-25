ca219343994ebbbe933754af80a107c2f0b224b0
SindreN <sind_nj@hotmail.com>
2013-09-25 12:16:12 +0200

    Testds

--- Pycharm FLASK.py
+++ Pycharm FLASK.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Woerld! FUCKING IDIOT'
+    return 'Hello Woerld! FUCKING IDIOTsd0ifjosdif'
 
 
 if __name__ == '__main__':
